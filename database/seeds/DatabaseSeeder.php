b41c586a7f365cbda20a9326b3f8ef5fbdc35a23
Rob Howdle <droidbuilderwebteam@gmail.com>
2020-08-10 01:05:17 +0100

    Remade Migrations and seeders for Droids, Custom Options, User-Admin. Parts Missing and error on adding droid to user

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,9 +12,10 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(RolesTableSeeder::class);
-        // $this->call(UsersTableSeeder::class);
-        // $this->call(DroidsTableSeeder::class);
+        $this->call(UsersTableSeeder::class);
+        $this->call(DroidsTableSeeder::class);
         // $this->call(PartsSeeder::class);
         // $this->call(DroidDetailsSeeder::class);
+         $this->call(CustomOptionList::class);
     }
 }
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,38 +14,12 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        User::truncate();
-        DB::table('role_user')->truncate();
-
-        $adminRole = Role::where('name', 'admin')->first();
-        $designerRole = Role::where('name', 'designer')->first();
-        $userRole = Role::where('name', 'user')->first();
-
-
         $admin = User::create([
-            'name' => 'Admin User',
+            'fname' => 'Admin',
+            'lname' => 'User',
+            'uname' => 'Administrator',
             'email' => 'admin@admin.com',
-            'password' => Hash::make('Skywalker94'),
+            'password' => Hash::make('Password1'),
         ]);
-        $designer = User::create([
-            'name' => 'Designer User',
-            'email' => 'designer@designer.com',
-            'password' => Hash::make('Skywalker94'),
-        ]);
-        $user = User::create([
-            'name' => 'User',
-            'email' => 'user@user.com',
-            'password' => Hash::make('Skywalker94'),
-        ]);
-        $rob = User::create([
-            'name' => 'Rob',
-            'email' => 'robhowdle94@gmail.com',
-            'password' => Hash::make('Skywalker94'),
-        ]);
-
-        $admin->roles()->attach($adminRole);
-        $designer->roles()->attach($designerRole);
-        $user->roles()->attach($userRole);
-        $rob->roles()->attach($adminRole);
     }
 }

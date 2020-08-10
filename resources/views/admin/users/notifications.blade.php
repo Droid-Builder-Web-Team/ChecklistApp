d03b156d83f6a142bce356e6d292aa167ee366ab
Rob Howdle <droidbuilderwebteam@gmail.com>
2020-08-10 23:30:29 +0100

    Fixed cockup

--- resources/views/admin/users/notifications.blade.php
+++ resources/views/admin/users/notifications.blade.php
@@ -7,12 +7,6 @@
         Notifications
     </div>
     <div class="card-body">
-        <form action="" method="post">
-            @if()
-            <input class="btn btn-primary float-right" value="Trigger" type="submit" name="user" value="{{ $user->id }}">
-        </form>
-    </div>
-    <div class="card-body">
         @foreach(Auth::user()->notifications as $notification)
             <h5><a href="admin/users/{{ $notification->data['user_id'] }}/profile">{{ $notification->data['user_name'] }} has landed.</a></h5>
             <p>{{ $notification->created_at->diffForHumans() }}</p>

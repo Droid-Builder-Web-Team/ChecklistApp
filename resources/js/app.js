6ff2df15aeea914a3c0a15a496af977562efb791
Rob Howdle <droidbuilderwebteam@gmail.com>
2020-09-14 23:08:34 +0100

    Getting Started Page. Bug on Scroll

--- resources/js/app.js
+++ resources/js/app.js
@@ -281,8 +281,11 @@ jQuery(function ($)
     {
         $(".page-wrapper").addClass("toggled");
     });
-
-
-
-
 });
+
+$(function() {
+    $('a[href*=#]').on('click', function(e) {
+      e.preventDefault();
+      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+    });
+  });

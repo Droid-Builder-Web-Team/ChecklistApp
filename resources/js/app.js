1631a1f158d70e8c0c81e86c2e39af12d2a0d2ec
Rob Howdle <droidbuilderwebteam@gmail.com>
2020-09-14 23:38:14 +0100

    Getting Started Page

--- resources/js/app.js
+++ resources/js/app.js
@@ -283,9 +283,4 @@ jQuery(function ($)
     });
 });
 
-$(function() {
-    $('a[href*=#]').on('click', function(e) {
-      e.preventDefault();
-      $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
-    });
-  });
+

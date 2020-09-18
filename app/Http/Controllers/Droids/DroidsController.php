5749a8c4da83bab3eff7eef60a46cb6640009551
Rob Howdle <droidbuilderwebteam@gmail.com>
2020-09-19 00:26:45 +0100

    Reworking Add Droid Form

--- app/Http/Controllers/Droids/DroidsController.php
+++ app/Http/Controllers/Droids/DroidsController.php
@@ -8,6 +8,7 @@ use App\Part;
 use App\Role;
 use App\Droid;
 use App\DroidUser;
+use Validator;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
@@ -87,62 +88,37 @@ class DroidsController extends Controller
      */
     public function store(Request $request)
     {
-        // if(Gate::denies('add-droids'))
-        // {
-        //     return redirect(route('droids.index.create'));
-        // }
+        //New Droid
+        $newDroid = new Droid();
+        $newDroid['class'] = $request->class;
+        $newDroid['description'] = $request->description;
+        $newDroid->save();
 
-        // if(Gate::denies('delete-droids'))
-        // {
-        //     return redirect(route('admin.users.index'));
-        // }
-
-        //Get and upload droid image
-        $request->validate(['image' => 'required|mimes:png,jpeg,jpg,gif|max:2048',]); //2mb limit (confirm?)
-            //$file->getSize(); may use to warn user... todo?
-        $file = $request->file('image'); //get the image
-        $newImageName = $file->getClientOriginalName(); //Not re-nameing!! Could overwrite!
-        $request->image->move(public_path('/img/'), $newImageName); //copy to public folder
-
-
-        //Stores new Droid to droids table
-        $newClass = new Droid;
-        $newClass['class'] = $request->class;
-        $newClass['description'] = $request->description;
-        $newClass['image'] = '/img/'. $newImageName;  //name from file plus local path folder
-        $newClass->save();
-
-        //Get CSV and move to temp folder
-       // $request->validate(['partslist' => 'required|mimes:csv, xlsx',]); //doesnt work??
-        $csv = $request->file('partslist'); //get the partlist spreadsheet
-        $newImageName = $csv->getClientOriginalName();
-        $request->partslist->move(public_path('/temp/'), $newImageName); //copy to public folder with new name
-
-        //get the new droidid from the newclass->save
-        $newDroid_ID = $newClass->id;
-
-        //Open the CSV for reading
-        $file=fopen(public_path('/temp/'.$newImageName),'r');
-
-        //loop through all records - saves one record at a time - slow...
-        while (($data = fgetcsv($file, 1000, ",")) !== FALSE)
-        {
-            //No validation! assumes CSV is correctly formatted.
-            $newPart = new Part;
-            $newPart['droids_id'] = $newDroid_ID; //field in CSV ignored, replaced by new droid id from above insert
-            $newPart['droid_version'] = $data[1];
-            $newPart['droid_section'] = $data[2];
-            $newPart['sub_section'] = $data[3];
-            $newPart['part_name'] = $data[4];
-            $newPart['file_path'] = $data[5];
-            $newPart->save();
-
+        // Droid Image Upload
+        if($request->hasFile('image')){
+            $filename = $request->image->getClientOriginalName();
+            $request->image->storeAs('img', $filename, 'public');
+            auth()->user()->update(['image' => '/img/' . $filename]);
         }
 
-        //delete temp CSV file
-        unlink(public_path('/temp/'.$newImageName)) or die("Couldn't delete file");
+        $validator = Validator::make($request->all(), [
+            'partslist' => 'required|mimes:csv,txt'
+        ]);
 
-        return back()->withMessage('Added new Droid with parts');
+        if($validator->passes())
+        {
+            $dataTime = date('Ymd_His');
+            $file = $request->file('partslist');
+            $fileName = $dataTime . '-' . $file->getClientOriginalName();
+            $savePath = public_path('/csvs/');
+            $file->move($savePath, $fileName);
+            return redirect()->back()
+                ->with(['success'=>'Filed Uploaded Successfully.']);
+
+        }else {
+            return redirect()->back()
+                ->with(['errors'=>$validator->errors()->all()]);
+        }
     }
 
     /**

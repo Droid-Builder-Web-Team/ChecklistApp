c9fc3c2961d7f5dc46d5e6b39858729bc6840818
Flex101 <matt_goodliffe@hotmail.com>
2020-10-02 21:51:53 +0100

    BT-1, R0, R2, R5, R6, and R7 CAD added to Custom Droid
    Removed R2MG and R7MG

--- database/seeds/CustomOptionList.php
+++ database/seeds/CustomOptionList.php
@@ -16,81 +16,81 @@ class CustomOptionList extends Seeder
             'class' => 'BT1',
             'version'=> 'MK3',
             'section' => 'Dome',
-            'frontImage' => '/img/BT1Dome.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/BT-1/Front_Dome.png',
+            'sideImageFore' => '/img/DroidParts/BT-1/Side_Dome.png',
             'sideImageBack' => NULL,
         ]);
         $R0 = CustomOption::create([
             'class' => 'R0',
             'version'=> 'MK3',
             'section' => 'Dome',
-            'frontImage' => '/img/R0Dome.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/R0/Front_Dome.png',
+            'sideImageFore' => '/img/DroidParts/R0/Side_Dome.png',
             'sideImageBack' => NULL,
         ]);
         $R2MK2D = CustomOption::create([
             'class' => 'R2',
             'version'=> 'MK2',
             'section' => 'Dome',
-            'frontImage' => '/img/R2DomeSplit.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/R2/Front_Dome.png',
+            'sideImageFore' => '/img/DroidParts/R2/Side_Dome.png',
             'sideImageBack' => NULL,
         ]);
         $R2MK2B = CustomOption::create([
             'class' => 'R2',
             'version'=> 'MK2',
             'section' => 'Body',
-            'frontImage' => '/img/R2Body.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/R2/Front_Body.png',
+            'sideImageFore' => '/img/DroidParts/R2/Side_Body.png',
             'sideImageBack' => NULL,
         ]);
         $R2MK2L = CustomOption::create([
             'class' => 'R2',
             'version'=> 'MK2',
             'section' => 'Legs',
-            'frontImage' => '/img/legsmk2.png',
-            'sideImageFore' => NULL,
-            'sideImageBack' => NULL,
+            'frontImage' => '/img/DroidParts/R2/Front_Legs.png',
+            'sideImageFore' => '/img/DroidParts/R2/Side_Legs.png',
+            'sideImageBack' => '/img/DroidParts/R2/Side_Legs_Back.png',
         ]);
         $R2MK2F = CustomOption::create([
             'class' => 'R2',
             'version'=> 'MK2',
             'section' => 'Feet',
-            'frontImage' => '/img/FootMK2.png',
-            'sideImageFore' => NULL,
-            'sideImageBack' => NULL,
+            'frontImage' => '/img/DroidParts/R2/Front_Feet.png',
+            'sideImageFore' => '/img/DroidParts/R2/Side_Feet.png',
+            'sideImageBack' => '/img/DroidParts/R2/Side_Feet_Back.png',
         ]);
         $R2MK3D = CustomOption::create([
             'class' => 'R2',
             'version'=> 'MK3',
             'section' => 'Dome',
-            'frontImage' => '/img/R2DomeSingle.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/R2/Front_Dome.png',
+            'sideImageFore' => '/img/DroidParts/R2/Side_Dome.png',
             'sideImageBack' => NULL,
         ]);
         $R2MK3B = CustomOption::create([
             'class' => 'R2',
             'version'=> 'MK3',
             'section' => 'Body',
-            'frontImage' => '/img/R2Body.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/R2/Front_Body.png',
+            'sideImageFore' => '/img/DroidParts/R2/Side_Body.png',
             'sideImageBack' => NULL,
         ]);
         $R2MK3L = CustomOption::create([
             'class' => 'R2',
             'version'=> 'MK3',
             'section' => 'Legs',
-            'frontImage' => '/img/legsmk3.png',
-            'sideImageFore' => NULL,
-            'sideImageBack' => NULL,
+            'frontImage' => '/img/DroidParts/R2/Front_Legs.png',
+            'sideImageFore' => '/img/DroidParts/R2/Side_Legs.png',
+            'sideImageBack' => '/img/DroidParts/R2/Side_Legs_Back.png',
         ]);
         $R2MK3F = CustomOption::create([
             'class' => 'R2',
             'version'=> 'MK3',
             'section' => 'Feet',
-            'frontImage' => '/img/FootMK3.png',
-            'sideImageFore' => NULL,
-            'sideImageBack' => NULL,
+            'frontImage' => '/img/DroidParts/R2/Front_Feet.png',
+            'sideImageFore' => '/img/DroidParts/R2/Side_Feet.png',
+            'sideImageBack' => '/img/DroidParts/R2/Side_Feet_Back.png',
         ]);
         $R4MK2 = CustomOption::create([
             'class' => 'R4',
@@ -112,64 +112,64 @@ class CustomOptionList extends Seeder
             'class' => 'R5MK2',
             'version'=> 'MK2',
             'section' => 'Dome',
-            'frontImage' => '/img/R5Dome.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/R5/Front_Dome.png',
+            'sideImageFore' => '/img/DroidParts/R5/Side_Dome.png',
             'sideImageBack' => NULL,
         ]);
         $R5MK3 = CustomOption::create([
             'class' => 'R5',
             'version'=> 'MK3',
             'section' => 'Dome',
-            'frontImage' => '/img/R5Dome.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/R5/Front_Dome.png',
+            'sideImageFore' => '/img/DroidParts/R5/Side_Dome.png',
             'sideImageBack' => NULL,
         ]);
         $R6MK2 = CustomOption::create([
             'class' => 'R6',
             'version'=> 'MK2',
             'section' => 'Dome',
-            'frontImage' => '/img/R6Dome.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/R6/Front_Dome.png',
+            'sideImageFore' => '/img/DroidParts/R6/Side_Dome.png',
             'sideImageBack' => NULL,
         ]);
         $R6MK3 = CustomOption::create([
             'class' => 'R6',
             'version'=> 'MK3',
             'section' => 'Dome',
-            'frontImage' => '/img/R6Dome.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/R6/Front_Dome.png',
+            'sideImageFore' => '/img/DroidParts/R6/Side_Dome.png',
             'sideImageBack' => NULL,
         ]);
         $R7MK2D = CustomOption::create([
             'class' => 'R7',
             'version'=> 'MK2',
             'section' => 'Dome',
-            'frontImage' => '/img/R7Dome.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/R7/Front_Dome.png',
+            'sideImageFore' => '/img/DroidParts/R7/Side_Dome.png',
             'sideImageBack' => NULL,
         ]);
         $R7MK3D = CustomOption::create([
             'class' => 'R7',
             'version'=> 'MK3',
             'section' => 'Dome',
-            'frontImage' => '/img/R7Dome.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/R7/Front_Dome.png',
+            'sideImageFore' => '/img/DroidParts/R7/Side_Dome.png',
             'sideImageBack' => NULL,
         ]);
         $R7MK2B = CustomOption::create([
             'class' => 'R7',
             'version'=> 'MK2',
             'section' => 'Body',
-            'frontImage' => '/img/R7Body.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/R7/Front_Body.png',
+            'sideImageFore' => '/img/DroidParts/R7/Side_Body.png',
             'sideImageBack' => NULL,
         ]);
         $R7MK3B = CustomOption::create([
             'class' => 'R7',
             'version'=> 'MK3',
             'section' => 'Body',
-            'frontImage' => '/img/R7Body.png',
-            'sideImageFore' => NULL,
+            'frontImage' => '/img/DroidParts/R7/Front_Body.png',
+            'sideImageFore' => '/img/DroidParts/R7/Side_Body.png',
             'sideImageBack' => NULL,
         ]);
         $R9MK2 = CustomOption::create([
@@ -186,54 +186,6 @@ class CustomOptionList extends Seeder
             'section' => 'Dome',
             'frontImage' => '/img/R9Dome.png',
             'sideImageFore' => NULL,
-            'sideImageBack' => NULL,
-        ]);
-        $R2MGD = CustomOption::create([
-            'class' => 'R2',
-            'version'=> 'MG',
-            'section' => 'Dome',
-            'frontImage' => '/img/DroidParts/R2MG/Front_Dome.png',
-            'sideImageFore' => '/img/DroidParts/R2MG/Side_Dome.png',
-            'sideImageBack' => NULL,
-        ]);
-        $R2MGB = CustomOption::create([
-            'class' => 'R2',
-            'version'=> 'MG',
-            'section' => 'Body',
-            'frontImage' => '/img/DroidParts/R2MG/Front_Body.png',
-            'sideImageFore' => '/img/DroidParts/R2MG/Side_Body.png',
-            'sideImageBack' => NULL,
-        ]);
-        $R2MGL = CustomOption::create([
-            'class' => 'R2',
-            'version'=> 'MG',
-            'section' => 'Legs',
-            'frontImage' => '/img/DroidParts/R2MG/Front_Legs.png',
-            'sideImageFore' => '/img/DroidParts/R2MG/Side_Legs.png',
-            'sideImageBack' => '/img/DroidParts/R2MG/Side_Legs_Back.png',
-        ]);
-        $R2MGF = CustomOption::create([
-            'class' => 'R2',
-            'version'=> 'MG',
-            'section' => 'Feet',
-            'frontImage' => '/img/DroidParts/R2MG/Front_Feet.png',
-            'sideImageFore' => '/img/DroidParts/R2MG/Side_Feet.png',
-            'sideImageBack' => '/img/DroidParts/R2MG/Side_Feet_Back.png',
-        ]);
-        $R7MGD = CustomOption::create([
-            'class' => 'R7',
-            'version'=> 'MG',
-            'section' => 'Dome',
-            'frontImage' => '/img/DroidParts/R7MG/Front_Dome.png',
-            'sideImageFore' => '/img/DroidParts/R7MG/Side_Dome.png',
-            'sideImageBack' => NULL,
-        ]);
-        $R7MGB = CustomOption::create([
-            'class' => 'R7',
-            'version'=> 'MG',
-            'section' => 'Body',
-            'frontImage' => '/img/DroidParts/R7MG/Front_Body.png',
-            'sideImageFore' => '/img/DroidParts/R7MG/Side_Body.png',
             'sideImageBack' => NULL,
         ]);
     }

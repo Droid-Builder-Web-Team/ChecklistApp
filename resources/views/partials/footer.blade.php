c17c26061f1167a8471ccec70bfc38f546aae30a
unknown <robhowdle94@gmail.com>
2020-11-04 12:48:51 +0000

    Added file size limit label

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -51,7 +51,7 @@
             <div class="col-md-4 col-sm-6 col-xs-12">
             <ul class="social-icons">
                 <li><a class="facebook" href="https://www.facebook.com/DroidBuilderWebTeam"><i class="fab fa-facebook"></i></a></li>
-                <li><a class="youtube" href="#"><i class="fab fa-youtube"></i></a></li>
+                <li><a class="youtube" href="https://youtube.com/channel/UCwT5SV9ezzcorQa9AHu11uQ"><i class="fab fa-youtube"></i></a></li>
                 <li><a class="discord" href="https://discord.gg/4ZV9DmY"><i class="fab fa-discord"></i></a></li>
                 <li><a class="email" href="mailto:droidbuilderwebteam@gmail.com"><i class="fas fa-envelope"></i></a></li>
             </ul>
